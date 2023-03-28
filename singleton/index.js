be44375f11b1a5ca2805ea5d46e4c2be3f2a830a
David Camacho <jossedaviid1@gmail.com>
2023-03-27 21:56:54 -0500

    add: Singleton with JS.

---
+++ singleton/index.js
@@ -0,0 +1,30 @@
+/**
+  * How to implement singleton?
+  * 0. Create falsy value for validate instance
+  * 1. Make the constructor private
+  * 2. Create a static method who calls the private constructor and save the instasnce in a static variable
+*/
+
+class SingletonClass {
+  static instance = undefined;
+
+  constructor(version) {
+    this.version = this.version;
+  }
+
+  static getInstance(version) {
+    if (!SingletonClass.instance) {
+      SingletonClass.instance = new SingletonClass(version);
+    }
+    return SingletonClass.instance;
+  }
+}
+
+function appSingleton() {
+  const singleton1 = SingletonClass.getInstance('v1');
+  const singleton2 = SingletonClass.getInstance('v2');
+
+  console.log(singleton1 === singleton2);
+}
+
+appSingleton();
